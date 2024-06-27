eb5f7abae4dd332bbca90dec8629eed6f6498ce9
Thomas Leonard <talex5@gmail.com>
2024-06-27 15:16:59 +0100

    Add fork mode
    Makes it easier to compare processes vs multicore.

--- stress/stress.ml
+++ stress/stress.ml
@@ -5,14 +5,27 @@ let packages = Packages.of_commit "../opam-repository/packages"
 
 let request = [OpamPackage.Name.of_string "0install-solver"]
 
-let rec run_worker packages todo =
-  if Atomic.fetch_and_add todo (-1) > 0 then (
+let run_worker packages n =
+  for _i = 1 to n do
     ignore @@ Domain_worker.solve packages request;
-    run_worker packages todo
-  )
+  done
 
-let main n_workers count =
-  let todo = Atomic.make count in
+let spawn_child fn =
+  match Unix.fork () with
+  | 0 -> fn (); exit 0
+  | child ->
+    fun () ->
+      match Unix.waitpid [] child with
+      | _, WEXITED 0 -> ()
+      | _ -> failwith "Child failed!"
+
+let spawn_domain fn =
+  let d = Domain.spawn fn in
+  fun () -> Domain.join d
+
+let main n_workers count fork =
+  Format.printf "Running in %s mode@." (if fork then "fork" else "domain");
+  let requests = count * n_workers in
   (* Warm-up *)
   let before = Unix.gettimeofday () in
   let expected = Domain_worker.solve packages request in
@@ -20,14 +33,17 @@ let main n_workers count =
   let time = Unix.gettimeofday () -. before in
   Format.printf "@.Solved warm-up request in: %.2fs@." time;
   (* Main run *)
-  Format.printf "Running another %d solves...@." count;
+  Format.printf "Running another %d * %d solves...@." count n_workers;
   let before = Unix.gettimeofday () in
-  let domains = List.init n_workers (fun _ -> Domain.spawn (fun () -> run_worker packages todo)) in
-  List.iter Domain.join domains;
+  let domains =
+    let spawn = if fork then spawn_child else spawn_domain in
+    List.init n_workers (fun _ -> spawn (fun () -> run_worker packages count))
+  in
+  List.iter (fun f -> f ()) domains;
   let time = Unix.gettimeofday () -. before in
-  let rate = float count /. time in
+  let rate = float requests /. time in
   Format.printf "@.Solved %d requests in %.2fs (%.2fs/iter) (%.2f solves/s)@."
-    count time (time /. float count) rate;
+    requests time (time /. float requests) rate;
   Fmt.pr "Workers, Rate@.";
   Fmt.pr "%d, %.3f@." n_workers rate
 
@@ -42,12 +58,17 @@ let internal_workers =
 let count =
   Arg.value
   @@ Arg.opt Arg.int 3
-  @@ Arg.info ~doc:"The number of requests to send" ~docv:"N" [ "count" ]
+  @@ Arg.info ~doc:"The number of requests to send per worker" ~docv:"N" [ "count" ]
+
+let fork =
+  Arg.value
+  @@ Arg.flag
+  @@ Arg.info ~doc:"Fork instead of using domains" [ "fork" ]
 
 let stress_local =
   let doc = "Run jobs using an in-process solver" in
   let info = Cmd.info "local" ~doc in
-  Cmd.v info Term.(const main $ internal_workers $ count)
+  Cmd.v info Term.(const main $ internal_workers $ count $ fork)
 
 let () =
   exit @@ Cmd.eval @@ stress_local
